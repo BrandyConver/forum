a83b45593bae26834688fd5d6879bd72fbe4fa39
BrandyConver <w821082263@163.com>
2018-09-05 14:29:27 +0800

    forum

---
+++ my_modules/myfun.js
@@ -0,0 +1,22 @@
+// 公有模块 md5 数据库
+const crypto=require('crypto');
+const mysql=require('mysql');
+const pool=mysql.createPool({host:'localhost',user:'root',password:'adgjl193726845',database:'schema1'});
+module.exports={
+	sign(str){
+		var key='keys';
+		var md5obj=crypto.createHash('md5');
+		return md5obj.update(str+key).digest('hex');
+	},
+	query(sql,cb){
+		pool.getConnection(function(err,connection){
+			if(err) throw err;
+			connection.query(sql,function(err,data){
+				if(err) throw err;
+				cb(data);
+				connection.release();
+			});
+		});
+
+	}
+}
